c467764f5566426bb19dec3756e1da4e2c9abfe6
Paolo Simonazzi <p.simonazzi@gmail.com>
2013-03-24 17:40:54 +0100

    Removed memoize

--- app/lib/utils.js
+++ app/lib/utils.js
@@ -63,39 +63,6 @@ exports.extend = function extend(a, b) {
 
 
 /**
-* Cache the given function's return value.
-* Instead of:
-* var result = fn(x);
-*
-* call the function to memoize as:
-* var memoizedFn = memoize(fn);
-* var result = memoizedFn(x);
-*
-* by @philogb and @addyosmani
-* with further optimizations by @mathias
-* and @DmitryBaranovsk
-* perf tests: http://bit.ly/q3zpG3
-* Released under an MIT license.
-*/
-exports.memoize = function memoize(fn) {
-  return function () {
-    var args = Array.prototype.slice.call(arguments),
-        hash = "",
-        i = args.length;
-    currentArg = null;
-    while (i--) {
-      currentArg = args[i];
-      hash += (currentArg === Object(currentArg)) ?
-        JSON.stringify(currentArg) : currentArg;
-      fn.memoize || (fn.memoize = {});
-    }
-    return (hash in fn.memoize) ? fn.memoize[hash] :
-      fn.memoize[hash] = fn.apply(this, args);
-  };
-};
-
-
-/**
  * Emulate blocking sleep system call, hopefully for debugging purposes only.
  * As Node is single-threaded, this will block the ENTIRE NODE PROCESS.
  *
